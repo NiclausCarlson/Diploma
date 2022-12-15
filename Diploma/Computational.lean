2e3fb2f1d705623a1081295c6286f62d6ea15f56
NikclausCarlon <you@example.com>
2022-12-15 23:23:02 +0300

    start write parser

--- Diploma/Computational.lean
+++ Diploma/Computational.lean
@@ -7,11 +7,18 @@ namespace Computational
     div: α → α → α 
     pow: α → Nat → α 
 
-  infixl:65 " + "   => Computational.sum
-  infixl:65 " - "   => Computational.sub
-  infixl:70 " * "   => Computational.mul
-  infixl:70 " / "   => Computational.div
-  infixr:80 " ^ "   => Computational.pow
+  infixl:65 " + " => Computational.sum
+  infixl:65 " - " => Computational.sub
+  infixl:70 " * " => Computational.mul
+  infixl:70 " / " => Computational.div
+  infixr:80 " ^ " => Computational.pow
+
+  instance: Computational Nat where
+    sum x y := x + y
+    sub x y := x - y
+    mul x y := x * y
+    div x y := x / y
+    pow x y := x ^ y
 
   instance: Computational Int where
     sum x y := x + y
