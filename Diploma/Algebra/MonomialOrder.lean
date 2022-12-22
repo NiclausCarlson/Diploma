6227a3ab642d5ba129d1ba4db1e30557beb45935
NikclausCarlon <you@example.com>
2022-12-22 15:40:21 +0300

    some updates

--- Diploma/Algebra/MonomialOrder.lean
+++ Diploma/Algebra/MonomialOrder.lean
@@ -1,18 +1,44 @@
+import Std.Data.RBTree
+
 import Diploma.Computational
 import Diploma.Polynomials.Polynomials
 
 namespace algebra
 open polynomials
 open computational
+  namespace orders
+    instance : Ord Variable where
+      compare v₁ v₂ := compare v₁.name v₂.name 
 
-  class LinearOrder (α: Type) [ToString α] [Inhabited α] [Computational α] :=
-    leq       : Polynomial α → Polynomial α → Prop
-    refl      : (∀ p     : Polynomial α, leq p p)
-    anty_symm : (∀ p q   : Polynomial α, leq p q → ((leq q p) = False))
-    trans     : (∀ m p q : Polynomial α, (leq m p) ∧ (leq p q) → (leq m q))
-    linear    : (∀ p q   : Polynomial α, (leq p q) ∨ (leq q p) = True)
+    private def VariablesToStr (m: List Variable) : String :=
+      match m with 
+        | []    => ""
+        | [v]   => v.name
+        | v::vs => v.name ++ VariablesToStr vs
 
-  class MonomialOrder (α: Type) [ToString α] [Inhabited α] [Computational α] :=
-    sorry
+    instance : Ord (List Variable) where
+      compare v₁ v₂ := compare (VariablesToStr v₁) (VariablesToStr v₂)
+
+    instance [ToString α] [Inhabited α] [Computational α]: Ord (Monomial α) where
+      compare m₁ m₂ := compare m₁.vars m₂.vars
+
+    
+
+    def lex [ToString α] [Inhabited α] [Computational α] (p q: Monomial α): Prop :=
+      sorry
+
+    def lex_ordering [ToString α] [Inhabited α] [Computational α] (l: List (Monomial α)): List (Monomial α) :=
+      sorry 
+
+    def p_lex_ordering [ToString α] [Inhabited α] [Computational α] (p: Polynomial α): Polynomial α :=
+      sorry 
+
+  end orders
+
+  class LinearOrder [ToString α] [Inhabited α] [Computational α] (φ : Monomial α → Monomial α → Prop)  :=
+    refl      : (∀ p     : Monomial α, φ p p)
+    anty_symm : (∀ p q   : Monomial α, φ p q → ((φ q p) = False))
+    trans     : (∀ m p q : Monomial α, (φ m p) ∧ (φ p q) → (φ m q))
+    linear    : (∀ p q   : Monomial α, (φ p q) ∨ (φ q p) = True)
 
 end algebra
--- Diploma/Polynomials/Polynomials.lean
+++ Diploma/Polynomials/Polynomials.lean
@@ -23,49 +23,49 @@ namespace ToString
   instance : ToString Variable where
     toString v := v.name ++ if v.deg != 1 then "^" ++ toString v.deg else ""
 
-  protected def VariablesToString (vs: List Variable): String :=
+  private def VariablesToString (vs: List Variable): String :=
     match vs with 
       | [] => ""
       | [a] => toString a
-      | a₁ :: as => (toString a₁) ++ (ToString.VariablesToString as)
+      | a₁ :: as => (toString a₁) ++ (VariablesToString as)
 
-  instance [ToString α] [Inhabited α]  [Computational α]: ToString (Monomial α) where
+  instance [ToString α] [Inhabited α] [Computational α]: ToString (Monomial α) where
     toString m := (toString m.coeff) ++ (ToString.VariablesToString m.vars)
 
-  protected def MonomialsToString [ToString α] [Inhabited α] [Computational α] (monomials: List (Monomial α)): String :=
+  private def MonomialsToString [ToString α] [Inhabited α] [Computational α] (monomials: List (Monomial α)): String :=
      match monomials with 
       | [] => ""
       | [a] => toString a
-      | a₁ :: as => toString a₁ ++ " + " ++ (ToString.MonomialsToString as)
+      | a₁ :: as => toString a₁ ++ " + " ++ (MonomialsToString as)
 
   instance [ToString α] [Inhabited α] [Computational α] : ToString (Polynomial α) where
-    toString p := ToString.MonomialsToString p.monomials
+    toString p := MonomialsToString p.monomials
 
 end ToString                                   
 
-def var_sub [ToString α] [Inhabited α] [Computational α] (var : Variable) (vs: Std.HashMap String α) : α := 
-  (vs.find! var.name) ^ var.deg
+  def varSub [ToString α] [Inhabited α] [Computational α] (var : Variable) (vs: Std.HashMap String α) : α := 
+    (vs.find! var.name) ^ var.deg
 
-def monomial_sub_impl [ToString α] [Inhabited α] [Computational α] (f: List Variable) (vs: Std.HashMap String α) : α := 
+  def monomialSubImpl [ToString α] [Inhabited α] [Computational α] (f: List Variable) (vs: Std.HashMap String α) : α := 
+      match f with
+        | [] => panic! "Found empty variables list"
+        | a₁ :: as => ( varSub a₁ vs ) + (monomialSubImpl as vs)
+
+  def monomialSub [ToString α] [Inhabited α] [Computational α] (f: Monomial α) (vs: Std.HashMap String α) : α :=
+     f.coeff * monomialSubImpl f.vars vs
+
+  def polynomialSubImpl [ToString α] [Inhabited α] [Computational α] (f: List (Monomial α)) (vs: Std.HashMap String α) : α :=
     match f with
-      | [] => panic! "Found empty variables list"
-      | a₁ :: as => ( var_sub a₁ vs ) + (monomial_sub_impl as vs)
+        | [] => panic! "Found empty monomials list"
+        | a₁ :: as => (monomialSub a₁ vs) + (polynomialSubImpl as vs)
 
-def monomial_sub [ToString α] [Inhabited α] [Computational α] (f: Monomial α) (vs: Std.HashMap String α) : α :=
-   f.coeff * monomial_sub_impl f.vars vs
+  def polynomialSub [ToString α] [Inhabited α] [Computational α] (f: Polynomial α) (vs: Std.HashMap String α) : α :=
+      polynomialSubImpl f.monomials vs
 
-def polynomial_sub_impl [ToString α] [Inhabited α] [Computational α] (f: List (Monomial α)) (vs: Std.HashMap String α) : α :=
-  match f with
-      | [] => panic! "Found empty monomials list"
-      | a₁ :: as => (monomial_sub a₁ vs) + (polynomial_sub_impl as vs)
-
-def polynomial_sub [ToString α] [Inhabited α] [Computational α] (f: Polynomial α) (vs: Std.HashMap String α) : α :=
-    polynomial_sub_impl f.monomials vs
-    
-#eval var_sub {deg:=5, name:="x"} (Std.HashMap.ofList [("x", 5)])
-#eval monomial_sub {coeff:=5, vars:=[{deg:=5, name:="x"}]}
-                 (Std.HashMap.ofList [("x", 5)])
-#eval polynomial_sub {monomials:=[{coeff:=5,
+  #eval varSub {deg:=5, name:="x"} (Std.HashMap.ofList [("x", 5)])
+  #eval monomialSub {coeff:=5, vars:=[{deg:=5, name:="x"}]}
+                   (Std.HashMap.ofList [("x", 5)])
+  #eval polynomialSub {monomials:=[{coeff:=5,
                                     vars:=[{deg:=5, name:="x"}]
                                     }]} (Std.HashMap.ofList [("x", 5)])
 
