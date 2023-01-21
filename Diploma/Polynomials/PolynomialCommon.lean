159c45f660c25e7e03a5498a5015cce3491d3ad3
NikclausCarlon <you@example.com>
2023-01-22 00:35:18 +0300

    add forgotten file

---
+++ Diploma/Polynomials/PolynomialCommon.lean
@@ -0,0 +1,15 @@
+import Mathlib.Data.Vector
+
+namespace polynomial
+
+abbrev Variables (n: Nat) := Vector Nat n
+
+def get_variables (n: Nat) : Variables n := (Vector.ofFn (fun _:Fin n => 0))
+
+open Vector in
+def Variables.mul (v₁ v₂: Variables n): Variables n :=
+  map₂ (fun x y => x + y) v₁ v₂
+
+abbrev Monomial (n: Nat) := Rat × (Variables n) 
+
+end polynomial
