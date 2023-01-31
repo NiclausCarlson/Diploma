6d22f589ad0b5315584c7061d68bf3be995ba2d1
NikclausCarlon <you@example.com>
2023-01-31 23:19:30 +0300

    some proofs in monomial order

--- Diploma/Algebra/MonomialOrder.lean
+++ Diploma/Algebra/MonomialOrder.lean
@@ -10,11 +10,11 @@ open Vector
 open polynomial
 open Classical
 
-def Order.lex (m₁ m₂: Monomial n): Prop := if m₁.snd = m₂.snd then True 
-                                           else cmp diff
+def Order.lex (vs₁ vs₂: Variables n): Prop := if vs₁ = vs₂ then True 
+                                            else cmp diff
   where 
     diff : List Int := 
-      (map₂ (fun x y => (Int.ofNat x) - (Int.ofNat y)) m₁.snd m₂.snd).toList
+      (map₂ (fun x y => (Int.ofNat x) - (Int.ofNat y)) vs₁ vs₂).toList
     cmp (l: List Int): Prop :=
       match l with
         | [] => True
@@ -23,40 +23,53 @@ def Order.lex (m₁ m₂: Monomial n): Prop := if m₁.snd = m₂.snd then True
                    else if x > 0 then True
                    else False
 
-instance (m₁ m₂: Monomial n): Decidable (Order.lex m₁ m₂) := by sorry
+instance (vs₁ vs₂: Variables n): Decidable (Order.lex vs₁ vs₂) := by sorry
   
 
 def Ordering.lex (m₁ m₂: Monomial n): Ordering := 
   if m₁.snd = m₂.snd then Ordering.eq
-  else if Order.lex m₁ m₂ then Ordering.gt
+  else if Order.lex m₁.snd m₂.snd then Ordering.gt
   else Ordering.lt
 
 
-theorem lex_le_refl : ∀ (a : Monomial n), Order.lex a a := by
+theorem lex_le_refl : ∀ (a : Variables n), Order.lex a a := by
   intro a
   simp [Order.lex]
   
-theorem lex_le_trans : ∀ (a b c : Monomial n), Order.lex a b → Order.lex b c → Order.lex a c := by
+  
+theorem lex_le_trans : ∀ (a b c : Variables n), Order.lex a b → Order.lex b c → Order.lex a c := by
   simp [Order.lex]
-  intros _ a _ b _ c h₁ h₂
+  intros a b c h₁ h₂
   split at h₁; split at h₂
   split; simp
   sorry
   split; simp; simp [Order.lex.diff, map₂]
   sorry
   sorry
-
   
-theorem lex_le_antisymm : ∀ (a b : Monomial n), Order.lex a b → Order.lex b a → a = b := by
+
+theorem lex_le_antisymm : ∀ (a b : Variables n), Order.lex a b → Order.lex b a → a = b := by
+  simp [Order.lex]
   intros a b h₁ h₂
+  split at h₁
+  split at h₂
   sorry
+  sorry
+  split at h₂
+  sorry
+  sorry
+  
 
-theorem lex_le_total : ∀ (a b : Monomial n), Order.lex a b ∨ Order.lex b a := by
+theorem lex_le_total : ∀ (a b : Variables n), Order.lex a b ∨ Order.lex b a := by
+  simp [Order.lex]
   intros a b
+  split; simp
+  split; simp
   sorry
+  
 
-instance LexOrder: LinearOrder (Monomial n) where
-  le m₁ m₂ := Order.lex m₁ m₂
+instance LexOrder: LinearOrder (Variables n) where
+  le vs₁ vs₂ := Order.lex vs₁ vs₂
   le_refl := lex_le_refl
   le_trans := lex_le_trans
   le_antisymm := lex_le_antisymm
@@ -65,20 +78,19 @@ instance LexOrder: LinearOrder (Monomial n) where
   decidable_eq := sorry
 
 
-
-def Order.grlex (m₁ m₂: Monomial n): Prop := 
-  if elem_sum m₁ > elem_sum m₂ then True
-  else if elem_sum m₁ = elem_sum m₂ then Order.lex m₁ m₂
+def Order.grlex (vs₁ vs₂: Variables n): Prop := 
+  if elem_sum vs₁ > elem_sum vs₂ then True
+  else if elem_sum vs₁ = elem_sum vs₂ then Order.lex vs₁ vs₂
   else False
   where
-    elem_sum (m: Monomial n): Nat :=
-      List.foldl (fun x y => x + y) 0 m.snd.toList
+    elem_sum (vs: Variables n): Nat :=
+      List.foldl (fun x y => x + y) 0 vs.toList
 
-instance (m₁ m₂: Monomial n): Decidable (Order.grlex m₁ m₂) := sorry
+instance (vs₁ vs₂: Variables n): Decidable (Order.grlex vs₁ vs₂) := sorry
 
 def Ordering.grlex (m₁ m₂: Monomial n): Ordering :=
   if m₁.snd = m₂.snd then Ordering.eq
-  else if Order.grlex m₁ m₂ then Ordering.gt
+  else if Order.grlex m₁.snd m₂.snd then Ordering.gt
   else Ordering.lt
 
 end algebra
