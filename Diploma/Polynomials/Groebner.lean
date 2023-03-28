76a15127ddb7e80f2f73191cec14e5c2b7dc2abe
NikclausCarlon <you@example.com>
2023-03-28 22:29:35 +0300

    fix build after update lean and mathlib

--- Diploma/Polynomials/Groebner.lean
+++ Diploma/Polynomials/Groebner.lean
@@ -4,23 +4,23 @@ namespace polynomial
 open Nat
 
 --# Degs
-def Polynomial.multideg (p: Polynomial n cmp): Variables n := 
+def Polynomial.multideg (p: Polynomial n _cmp): Variables n := 
   if p.size != 0 then p.min!.snd
   else get_variables n 
 
-def Polynomial.lc (p: Polynomial n cmp): Rat :=
+def Polynomial.lc (p: Polynomial n _cmp): Rat :=
   if p.size != 0 then p.min!.fst
   else 0
 
-def Polynomial.lm (p: Polynomial n cmp): Monomial n :=
+def Polynomial.lm (p: Polynomial n _cmp): Monomial n :=
   if p.size != 0 then (1, p.min!.snd)
   else (1, get_variables n) 
 
-def Polynomial.lt (p: Polynomial n cmp): Monomial n :=
+def Polynomial.lt (p: Polynomial n _cmp): Monomial n :=
   if p.size != 0 then p.min!
   else (1, get_variables n)
 
-def Polynomial.Lt (p: Polynomial n cmp): Polynomial n cmp :=
+def Polynomial.Lt (p: Polynomial n _cmp): Polynomial n _cmp :=
   if p.size != 0 then Polynomial.single p.min!
   else Polynomial.single (1, get_variables n)
 
@@ -37,31 +37,31 @@ open Vector in
 private def Variables.div (v₁ v₂: Variables n) : Variables n := map₂ (fun x y: Nat => x - y) v₁ v₂
 def Monomial.div (m₁ m₂: Monomial n): Monomial n := (m₁.fst / m₂.fst, Variables.div m₁.snd m₂.snd)  
 
-structure ReduceResult (n: Nat) (cmp: Monomial n → Monomial n → Ordering) where
-  reduced: Polynomial n cmp
-  reducer: Polynomial n cmp
+structure ReduceResult (n: Nat) (_cmp: Monomial n → Monomial n → Ordering) where
+  reduced: Polynomial n _cmp
+  reducer: Polynomial n _cmp
 
 --# Reduce p₁ by p₂
-def reduce_lt (p₁ p₂: Polynomial n cmp): Option (ReduceResult n cmp) := 
+def reduce_lt (p₁ p₂: Polynomial n _cmp): Option (ReduceResult n _cmp) := 
   if p₁.lt.is_div p₂.lt then some (impl (Polynomial.single (p₁.lt.div p₂.lt)))
   else none
   where
-    impl (p: Polynomial n cmp): ReduceResult n cmp := 
+    impl (p: Polynomial n _cmp): ReduceResult n _cmp := 
       let reducer := p * p₂
       {
         reduced := p₁ - reducer,
         reducer := reducer
       }
 
-structure DivisionResult (n: Nat) (cmp: Monomial n → Monomial n → Ordering) where
-  p: Polynomial n cmp
-  r: Polynomial n cmp
+structure DivisionResult (n: Nat) (_cmp: Monomial n → Monomial n → Ordering) where
+  p: Polynomial n _cmp
+  r: Polynomial n _cmp
 
-def divide_many (divisible: Polynomial n cmp) (dividers: List (Polynomial n cmp)): DivisionResult n cmp := 
+def divide_many (divisible: Polynomial n _cmp) (dividers: List (Polynomial n _cmp)): DivisionResult n _cmp := 
   if dividers == [] then {p:=0, r:=0}
   else impl divisible dividers {p:=0, r:=0}
   where
-    impl (p: Polynomial n cmp) (ps: List (Polynomial n cmp)) (step: DivisionResult n cmp): DivisionResult n cmp :=
+    impl (p: Polynomial n _cmp) (ps: List (Polynomial n _cmp)) (step: DivisionResult n _cmp): DivisionResult n _cmp :=
         if p == 0 then step
         else match ps with
               | []    => let r := step.r + p.Lt
@@ -79,19 +79,19 @@ def divide_many (divisible: Polynomial n cmp) (dividers: List (Polynomial n cmp)
 
 def Monomial.lcm (m₁ m₂: Monomial n): Monomial n := ⟨1, Vector.map₂ (fun x y => max x y) m₁.snd m₂.snd⟩  
 
-def build_s_polynomial (p₁ p₂: Polynomial n cmp): Polynomial n cmp :=
+def build_s_polynomial (p₁ p₂: Polynomial n _cmp): Polynomial n _cmp :=
   let lcm := Monomial.lcm (p₁.lm) (p₂.lm)
   (div_lcm_lt lcm p₁.lt) * p₁ - (div_lcm_lt lcm p₂.lt) * p₂
 where
-  div_lcm_lt (lcm lt: Monomial n): Polynomial n cmp := Polynomial.single (lcm.div lt)
+  div_lcm_lt (lcm lt: Monomial n): Polynomial n _cmp := Polynomial.single (lcm.div lt)
 
-private def step (p q: Polynomial n cmp) (ps: List (Polynomial n cmp)) : Bool × Polynomial n cmp := 
+private def step (p q: Polynomial n _cmp) (ps: List (Polynomial n _cmp)) : Bool × Polynomial n _cmp := 
   if p == q then (false, 0)
   else
     let div_result := divide_many (build_s_polynomial p q) ps
     (div_result.r == 0, div_result.r)
 
-private def build (e: Polynomial n cmp) (ps: List (Polynomial n cmp)) (dividers: List (Polynomial n cmp)) : List (Polynomial n cmp) := 
+private def build (e: Polynomial n _cmp) (ps: List (Polynomial n _cmp)) (dividers: List (Polynomial n _cmp)) : List (Polynomial n _cmp) := 
   match ps with
     | []    => []
     | a::as => let s := step e a dividers 
@@ -103,9 +103,9 @@ decreasing_by {
   sorry
 }
 
-def build_groebner_basis (pl: List (Polynomial n cmp)): List (Polynomial n cmp) := impl pl pl
+def build_groebner_basis (pl: List (Polynomial n _cmp)): List (Polynomial n _cmp) := impl pl pl
   where 
-    impl (pl: List (Polynomial n cmp)) (dividers: List (Polynomial n cmp)): List (Polynomial n cmp) := 
+    impl (pl: List (Polynomial n _cmp)) (dividers: List (Polynomial n _cmp)): List (Polynomial n _cmp) := 
       match pl with
         | []    => dividers 
         | p::ps => let res := build p ps dividers
@@ -116,12 +116,12 @@ def build_groebner_basis (pl: List (Polynomial n cmp)): List (Polynomial n cmp) 
       sorry
     }
 
-def is_in (p: Polynomial n cmp) (ps: List (Polynomial n cmp)) : Bool := 
+def is_in (p: Polynomial n _cmp) (ps: List (Polynomial n _cmp)) : Bool := 
   let basis := build_groebner_basis ps
   let div_res := divide_many p basis
   div_res.r == 0
 
-def is_in_basis (p: Polynomial n cmp) (groebner_basis: List (Polynomial n cmp)) : Bool :=
+def is_in_basis (p: Polynomial n _cmp) (groebner_basis: List (Polynomial n _cmp)) : Bool :=
   (divide_many p groebner_basis).r == 0
 
 end polynomial
--- Diploma/Polynomials/Polynomial.lean
+++ Diploma/Polynomials/Polynomial.lean
@@ -13,18 +13,18 @@ def NameShift := 120
 
 abbrev Polynomial (n: Nat) (cmp: Monomial n → Monomial n → Ordering) := Std.RBSet (Monomial n) cmp
 
-def Polynomial.single (m: Monomial n): Polynomial n cmp := Std.RBSet.single m 
+def Polynomial.single (m: Monomial n): Polynomial n _cmp := Std.RBSet.single m 
 
 instance: OfNat (Monomial n) m where
   ofNat := ⟨m, get_variables n⟩  
 
-instance: OfNat (Polynomial n cmp) m where
+instance: OfNat (Polynomial n _cmp) m where
   ofNat := Std.RBSet.single (m, (get_variables n))
 
-def Polynomial.Simplify (p: Polynomial n cmp) : Polynomial n cmp := 
+def Polynomial.Simplify (p: Polynomial n _cmp) : Polynomial n _cmp := 
   check_non_empty (Std.RBSet.filter p (fun x => x.fst != 0))
   where 
-    check_non_empty (p: Polynomial n cmp): Polynomial n cmp :=
+    check_non_empty (p: Polynomial n _cmp): Polynomial n _cmp :=
       if p.isEmpty then 0
       else p
 
@@ -35,37 +35,37 @@ def Polynomial.of_monomial (m: Monomial n) (cmp: Monomial n → Monomial n → O
   where 
     empty : Polynomial n cmp := {}
 
-def Polynomial.add (p₁ p₂: Polynomial n cmp) : Polynomial n cmp := 
+def Polynomial.add (p₁ p₂: Polynomial n _cmp) : Polynomial n _cmp := 
   Simplify (Std.RBSet.mergeWith (fun x y => (x.fst + y.fst, x.snd)) p₁ p₂)
 
-instance: HAdd (Polynomial n cmp) (Polynomial n cmp) (Polynomial n cmp) where
+instance: HAdd (Polynomial n _cmp) (Polynomial n _cmp) (Polynomial n _cmp) where
   hAdd p₁ p₂ := Polynomial.add p₁ p₂  
 
 def Monomial.mul (m₁ m₂: Monomial n) : Monomial n :=
   (m₁.fst * m₂.fst, Variables.mul m₁.snd m₂.snd)
 
-def Polynomial.mul_monomial (p : Polynomial n cmp) (m: Monomial n) : Polynomial n cmp :=
+def Polynomial.mul_monomial (p : Polynomial n _cmp) (m: Monomial n) : Polynomial n _cmp :=
   monomials_mul p.toList m
   where 
-    monomials_mul (monomials: List (Monomial n)) (m: Monomial n) : Polynomial n cmp :=
+    monomials_mul (monomials: List (Monomial n)) (m: Monomial n) : Polynomial n _cmp :=
       match monomials with
         | []     => 0
         | [m₁]   => Std.RBSet.single (Monomial.mul m₁ m)
         | m₁::ms => Polynomial.add (Std.RBSet.single (Monomial.mul m₁ m)) (monomials_mul ms m)
 
-def Polynomial.mul (p₁ p₂: Polynomial n cmp) : Polynomial n cmp := Simplify (monomials_mul p₁.toList p₂) 
+def Polynomial.mul (p₁ p₂: Polynomial n _cmp) : Polynomial n _cmp := Simplify (monomials_mul p₁.toList p₂) 
   where 
-    monomials_mul (monomials: List (Monomial n)) (p: Polynomial n cmp): Polynomial n cmp :=
+    monomials_mul (monomials: List (Monomial n)) (p: Polynomial n _cmp): Polynomial n _cmp :=
       match monomials with
         | []    => p
         | [m]   => Polynomial.mul_monomial p m
         | m::ms => Polynomial.add (Polynomial.mul_monomial p m) (monomials_mul ms p)
 
-instance: HMul (Polynomial n cmp) (Polynomial n cmp) (Polynomial n cmp) where
+instance: HMul (Polynomial n _cmp) (Polynomial n _cmp) (Polynomial n _cmp) where
   hMul p₁ p₂ := Polynomial.mul p₁ p₂
 
-instance: HSub (Polynomial n cmp) (Polynomial n cmp) (Polynomial n cmp) where
-  hSub p₁ p₂ := p₁ + ((ofRat n cmp (-1)) * p₂)
+instance: HSub (Polynomial n _cmp) (Polynomial n _cmp) (Polynomial n _cmp) where
+  hSub p₁ p₂ := p₁ + ((ofRat n _cmp (-1)) * p₂)
 
 section ToString
 
@@ -116,7 +116,7 @@ private def MonomialsListToString (l: List (Monomial n)) : String :=
       if x.fst >= 0 then "+"
       else ""
 
-instance: ToString (Polynomial n cmp) where
+instance: ToString (Polynomial n _cmp) where
   toString p := MonomialsListToString p.toList
 
 end ToString
