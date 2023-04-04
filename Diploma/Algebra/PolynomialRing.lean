0ea4fb23f1576c203e88e6dd73c6ad4b7af49c93
NikclausCarlon <you@example.com>
2023-04-04 18:28:36 +0300

    cast RbTree polynomial to AddMonoidAlgebra polynomial

--- Diploma/Algebra/PolynomialRing.lean
+++ Diploma/Algebra/PolynomialRing.lean
@@ -1,82 +1,32 @@
-import Mathlib.Data.Finset.Basic
-import Mathlib.Data.List.Sort
 import Mathlib.Data.MvPolynomial.Basic
+import Mathlib.Data.Fin.Basic
+import Mathlib.Data.MvPolynomial.Variables
 
 import Std.Data.List.Basic
 
 import Diploma.Polynomials.Polynomial
 import Diploma.Algebra.MonomialOrder
-import Diploma.tests.Common
 
 open polynomial
-open algebra
 open List
+open Vector
+open MvPolynomial
 
-def ms: Set Nat := {1, 2, 3}
-def q := MvPolynomial ms Rat
+noncomputable section 
 
-structure NonEmptyPolynomial (n : Nat) (_cmp: Monomial n -> Monomial n -> Ordering) where
-  poly: Polynomial n _cmp
-  non_empty: poly.isEmpty ==  false
+def zeroP (n: Nat): MvPolynomial (Fin n) Rat := 0 
+def oneP (n: Nat): MvPolynomial (Fin n) Rat := 1 
 
-instance: OfNat (NonEmptyPolynomial n _cmp) m where
-  ofNat := ⟨Std.RBSet.single (m, (get_variables n)), rfl⟩ 
+def toMvMonomialImpl (vs: Vector Nat n) (idx: Nat) (real_n: Nat) : MvPolynomial (Fin real_n) Rat :=
+  if h: idx < real_n then 
+    match vs with
+       | ⟨[], _⟩   => oneP real_n
+       | ⟨a::_, _⟩ => get_p ⟨idx, h⟩ a * (toMvMonomialImpl (vs.tail) (idx+1) real_n)  
+  else oneP real_n
+where
+  get_p (idx: Fin real_n) (deg: Nat): MvPolynomial (Fin real_n) Rat := (X idx) ^ deg
 
-private theorem non_empty_means_size_non_zero {p: Polynomial n _cmp} (h: p.isEmpty = false): p.size != 0:= by
-  rw [Std.RBSet.isEmpty] at h
-  split at h
-  contradiction
-  rename_i h₁
-  rw [Polynomial, Std.RBSet] at p
-  rename_i p₁ _
-  cases p₁
-  rename_i val prop
-  simp [Std.RBSet.size]
-  cases val
-  have t := h₁ prop
-  simp [t]
-  rfl
+def toMvMonomial (m: Monomial n) : MvPolynomial (Fin n) Rat := (C m.fst) * (toMvMonomialImpl m.snd 0 n)
 
-def NonEmptyPolynomial.add (p₁ p₂: NonEmptyPolynomial n _cmp) : NonEmptyPolynomial n _cmp :=
-  ⟨Polynomial.add p₁.poly p₂.poly,
-   by rw [Polynomial.add, Polynomial.Simplify, Polynomial.Simplify.check_non_empty]
-      split
-      rfl
-      simp at *
-      rename_i h
-      exact h
-    ⟩   
-
-theorem t_zero_add : ∀ (a : NonEmptyPolynomial n _cmp), NonEmptyPolynomial.add 0 a = a := by
-  intros p
-  cases p
-  rename_i poly _
-  simp [NonEmptyPolynomial.add, Polynomial.add, Polynomial.Simplify, Polynomial.Simplify.check_non_empty]
-  split
-  rename_i h
-  simp [Std.RBSet.mergeWith] at h
-  simp [Std.RBSet.find?, Std.RBNode.find?] at h
-  simp [Polynomial] at poly
-  simp [Polynomial]   
-  sorry
-  sorry
-  
-
-instance: CommRing (NonEmptyPolynomial n _cmp) where
-  add p₁ p₂ := NonEmptyPolynomial.add p₁ p₂
-  zero_add := sorry
-  add_zero := sorry
-  add_comm := sorry
-  add_assoc := sorry
-  mul := sorry
-  left_distrib := sorry
-  right_distrib := sorry
-  zero_mul := sorry
-  mul_zero := sorry
-  mul_assoc := sorry
-  one_mul := sorry
-  mul_one := sorry
-  neg := sorry
-  add_left_neg := sorry
-  mul_comm := sorry
-
+def toMvPolynomial (p: Polynomial n _cmp): MvPolynomial (Fin n) Rat :=
+  p.foldl (fun x y => x + toMvMonomial y) (zeroP n)
